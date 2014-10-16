34d359e59f7c0898acedc65fb5285a4b08aeec31
Robert Bikár <bobMaca@robert-mbp.lan>
2014-10-16 20:29:12 +0200

    ssss

---
+++ myFistGit/test/test/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  test
+//
+//  Created by Robert Bikár on 16.10.14.
+//  Copyright (c) 2014 Robert Bikár. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
